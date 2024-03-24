5dfa2d447fd2f81ee70a54efda28cd4bc7d6383e
Albert Alexander Jacobsz <74448653+albertjacobsz@users.noreply.github.com>
2024-03-24 09:04:59 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomeScreen from './Screen/HomeScreen';
 import Snap from './Screen/Snap';
 import './App.css';
-
+//forced comment
 function App() {
   return (
     <Router>
